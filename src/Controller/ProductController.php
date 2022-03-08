7a8b951cd89c845bb1b4c22eb71d9181b64a9727
StevyDevWeb <stevywebdev@gmail.com>
2022-03-08 16:38:47 +0100

    user connexion

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,19 +2,17 @@
 
 namespace App\Controller;
 
-use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/product', name: 'app_product_')]
 class ProductController extends AbstractController
 {
-    #[Route('/show', name: 'show')]
-    public function show(ProductRepository $products): Response
+    #[Route('/product', name: 'app_product')]
+    public function index(): Response
     {
-        return $this->render('product/show.html.twig', [
-            'products' => $products->findAll(),
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
         ]);
     }
 }
