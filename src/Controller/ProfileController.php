85cfba3047e9d88ecdc04c3d12bbaad610f8f910
StevyDevWeb <stevywebdev@gmail.com>
2022-03-08 16:19:27 +0100

    first

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/profile', name: 'app_profile_')]
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
+    #[Route('/profile', name: 'login')]
     public function index(): Response
     {
         return $this->render('profile/index.html.twig', [
