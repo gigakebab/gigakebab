2ae6394355085a1777dd8b3caddb3aefa6bdf24b
Tim Messaoudene <messaoudene-tim@outlook.fr>
2022-03-11 10:12:15 +0100

    Order

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -8,12 +8,14 @@ use App\Repository\ProductRepository;
 use App\Repository\UserRepository;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/order', name: 'order_')]
+#[IsGranted('ROLE_USER')]
 class OrderController extends AbstractController
 {
     #[Route('/create', name: 'app_create')]
